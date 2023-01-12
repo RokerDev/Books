33b6cfea764409fe5938d469f52b8674dfbaa865
RokerDev <rokerdev91@gmail.com>
2023-01-12 19:44:50 +0100

    inject UnitOfWork

--- Books/Program.cs
+++ Books/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<UnitOfWork, UnitOfWork>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<BooksContext>(
