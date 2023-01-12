0327584cc3009916fe28731e6d8877d146030d2e
RokerDev <rokerdev91@gmail.com>
2023-01-12 19:45:11 +0100

    add LibraryController

---
+++ Books/Controllers/LibraryController.cs
@@ -0,0 +1,19 @@
+﻿using Books.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        private UnitOfWork _unitOfWork;
+        public LibraryController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
