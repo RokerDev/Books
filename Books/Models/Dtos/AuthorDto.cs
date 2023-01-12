9f5414c606f507732cef0488f4f0fdd0bc2bbed0
RokerDev <rokerdev91@gmail.com>
2023-01-12 20:47:00 +0100

    add Author Dto model

---
+++ Books/Models/Dtos/AuthorDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Books.Models.Dtos
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public DateTime BirthDate { get; set; }
+
+        public bool Gender { get; set; }
+    }
+}
