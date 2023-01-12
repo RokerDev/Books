af30adb7c48a580e5f94e480609285b7eb13205c
RokerDev <rokerdev91@gmail.com>
2023-01-12 21:03:38 +0100

    add AuthorConverter

---
+++ Books/Models/Converters/AuthorConverter.cs
@@ -0,0 +1,41 @@
+﻿using Books.Models.Domains;
+using Books.Models.Dtos;
+
+namespace Books.Models.Converters
+{
+    public static class AuthorConverter
+    {
+        public static AuthorDto ToDto(this Author model)
+        {
+            return new AuthorDto
+            {
+                Id = model.Id,
+                BirthDate = model.BirthDate,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Gender = model.Gender
+            };
+        }
+
+        public static IEnumerable<AuthorDto> ToDtos(this IEnumerable<Author> model) 
+        {
+            if (model == null)
+            {
+                return Enumerable.Empty<AuthorDto>();
+            }
+            return model.Select(x => x.ToDto());
+        }
+
+        public static Author ToDao(this AuthorDto model) 
+        {
+            return new Author
+            {
+                Id = model.Id,
+                BirthDate = model.BirthDate,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Gender = model.Gender
+            };
+        }
+    }
+}
