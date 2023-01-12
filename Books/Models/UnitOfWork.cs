2ebd9e29bf52f218a2993dac52903fb7ceb2bdaa
RokerDev <rokerdev91@gmail.com>
2023-01-12 19:32:47 +0100

    add UnitOfWorks

---
+++ Books/Models/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Books.Models.Repositories;
+
+namespace Books.Models
+{
+    public class UnitOfWork
+    {
+        private BooksContext _context;
+        public UnitOfWork(BooksContext context)
+        {
+            _context = context;
+            Author = new AuthorRepository(context);
+        }
+
+        public AuthorRepository Author { get; set; }
+
+        public void Complete()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
