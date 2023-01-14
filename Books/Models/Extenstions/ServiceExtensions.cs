a5fefa7b384f835159467c334216ffa6deb83576
RokerDev <rokerdev91@gmail.com>
2023-01-14 11:40:22 +0100

    add new projects and extensions
    Contract project for an Abstract layer
    LoggerService for managing logs

---
+++ Books/Models/Extenstions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Books.Models.Extenstions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder => 
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyOrigin()); 
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
--- Books/Program.cs
+++ Books/Program.cs
@@ -1,9 +1,13 @@
 using Books.Models;
+using Books.Models.Extenstions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
 builder.Services.AddScoped<UnitOfWork, UnitOfWork>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,16 +15,19 @@ builder.Services.AddDbContext<BooksContext>(
         options => options.UseSqlServer("name=ConnectionStrings:BooksContext"));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
 
