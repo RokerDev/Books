fad5e366f38723802fb2f6522de41c0d2c492edb
RokerDev <rokerdev91@gmail.com>
2023-01-14 12:16:18 +0100

    add new ServiceExtension to configure logger service

---
+++ Books/Extenstions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace Books.Extenstions
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// allows all requests from all origins to be sent API
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyOrigin());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        /// <summary>
+        /// add the logger service inside the .Net Core's IOC container
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+
+}
