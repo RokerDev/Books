f9e14c082bd3d3f731f5d488fdbea7b70144e5e5
RokerDev <rokerdev91@gmail.com>
2023-01-14 19:15:29 +0100

    change RepositoryManager class

--- Repository/RepositoryManager.cs
+++ Repository/RepositoryManager.cs
@@ -1,17 +1,25 @@
-﻿namespace Repository
+﻿using Contracts;
+
+namespace Repository
 {
-    public class RepositoryManager
+    public class RepositoryManager : IRepositoryManager
     {
-        private BooksContext _context;
+        private readonly BooksContext _context;
+        private readonly Lazy<IAuthorRepository> _authorRepository;
+        private readonly Lazy<IBookAuthorRepository> _bookAuthorRepository;
+        private readonly Lazy<IBookRepository> _bookRepository;
+
         public RepositoryManager(BooksContext context)
         {
             _context = context;
-            Author = new AuthorRepository(context);
-            Book = new BookRepository(context);
+            _authorRepository = new Lazy<IAuthorRepository>(() => new AuthorRepository(context));
+            _bookAuthorRepository = new Lazy<IBookAuthorRepository>(() => new BookAuthorRepository(context));
+            _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(context));
         }
 
-        public AuthorRepository Author { get; set; }
-        public BookRepository Book { get; set; }
+        public IAuthorRepository Author => _authorRepository.Value;
+        public IBookAuthorRepository BookAuthor => _bookAuthorRepository.Value;
+        public IBookRepository Book => _bookRepository.Value;
 
         public void Complete()
         {
