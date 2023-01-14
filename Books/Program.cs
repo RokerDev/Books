3dd148bb2483e686abbcb4f3ac4696480a8de236
RokerDev <rokerdev91@gmail.com>
2023-01-14 12:28:36 +0100

    add log to program.cs

--- Books/Program.cs
+++ Books/Program.cs
@@ -1,21 +1,25 @@
+using Books.Extenstions;
 using Books.Models;
-using Books.Models.Extenstions;
 using Microsoft.EntityFrameworkCore;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 builder.Services.AddScoped<UnitOfWork, UnitOfWork>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddDbContext<BooksContext>(
         options => options.UseSqlServer("name=ConnectionStrings:BooksContext"));
 
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
