9f006793425be16e3b4c160b8da51bedd1241cee
RokerDev <rokerdev91@gmail.com>
2023-01-15 10:40:41 +0100

    add exception

---
+++ Entities/Exceptions/AuthorNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿public sealed class AuthorNotFoundException : NotFoundException
+{
+    public AuthorNotFoundException(int id)
+    : base($"The Author with id: {id} doesn't exist in the database.")
+    {
+    }
+}
